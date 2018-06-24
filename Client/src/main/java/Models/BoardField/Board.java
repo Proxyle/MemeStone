b18c1ccef441185aa27c3116cf401bc73c4e4640
chf3kl9 <17722335+chf3kl9@users.noreply.github.com>
2018-06-24 22:06:20 +0200

    Update Board.java

--- Client/src/main/java/Models/BoardField/Board.java
+++ Client/src/main/java/Models/BoardField/Board.java
@@ -2,13 +2,16 @@ package Models.BoardField;
 
 import Models.Card.Minion.Minion;
 
-public class Board {
+ublic class Board {
+    final static int MAX_MANA = 10;
     Field leftField;
     Field rightField;
     int upperHero;
     int lowerHero;
     int upperHeroMana;
     int lowerHeroMana;
+    int upperHeroMaxMana;
+    int lowerHeroMaxMana;
 
     public Board(int heroHealth){
         leftField = new Field();
@@ -17,19 +20,15 @@ public class Board {
         lowerHero = heroHealth;
     }
 
-    public Board(Field leftField, Field rightField, int upperHero, int lowerHero){
+    public Board(Field leftField, Field rightField, int upperHero, int upperHeroMana, int upperHeroMaxMana, int lowerHero, int lowerHeroMana, int lowerHeroMaxMana){
         this.leftField = leftField;
         this.rightField = rightField;
         this.upperHero = upperHero;
+        this.upperHeroMana = upperHeroMana;
+        this.upperHeroMaxMana = upperHeroMaxMana;
         this.lowerHero = lowerHero;
-    }
-
-    public Field getLeftField() {
-        return leftField;
-    }
-
-    public Field getRightField() {
-        return rightField;
+        this.lowerHeroMana = lowerHeroMana;
+        this.lowerHeroMaxMana = lowerHeroMaxMana;
     }
 
     public int getLowerHero() {
@@ -40,6 +39,10 @@ public class Board {
         return lowerHeroMana;
     }
 
+    public int getLowerHeroMaxMana(){
+        return lowerHeroMaxMana;
+    }
+
     public int getUpperHero() {
         return upperHero;
     }
@@ -48,25 +51,44 @@ public class Board {
         return upperHeroMana;
     }
 
+    public int getUpperHeroMaxMana(){
+        return upperHeroMaxMana;
+    }
+
+    public Field getLeftField() {
+        return leftField;
+    }
+
+    public Field getRightField() {
+        return rightField;
+    }
+
     public Board flipBoard(){
-        return new Board(rightField.flipField(), leftField.flipField(), lowerHero, upperHero);
+        return new Board(rightField.flipField(), leftField.flipField(), lowerHero, lowerHeroMana, lowerHeroMaxMana, upperHero, upperHeroMana, upperHeroMaxMana);
     }
 
     public boolean placeMinion(Minion minion, int location){
-        if (location > 3)
-            return rightField.placeMinion(minion, location-3);
-        return leftField.placeMinion(minion, location);
+        if (minion.getCost() > lowerHeroMana)
+            return false;
+        boolean success;
+        if (location > 2)
+            success = rightField.placeMinion(minion, location-3);
+        else
+            success = leftField.placeMinion(minion, location);
+        if (success)
+            lowerHeroMana-=minion.getCost();
+        return success;
     }
 
     public boolean attack(int attacker, int defender){
-        if (attacker > 3)
+        if (attacker > 2)
             return rightField.attack(attacker-3, defender-3);
         return leftField.attack(attacker, defender);
     }
 
     public boolean attackOpponentHero(int attacker){
         int damage;
-        if (attacker > 3)
+        if (attacker > 2)
             damage = rightField.getDamage(attacker-3);
         else
             damage = leftField.getDamage(attacker);
@@ -75,14 +97,20 @@ public class Board {
     }
 
     public boolean canAttackHero(int attacker){
-        if (attacker > 3)
+        if (attacker > 2)
             return rightField.isLaneClear();
         return leftField.isLaneClear();
     }
 
     public boolean damage(int location, int damage, boolean own){
-        if (location > 3)
+        if (location > 2)
             return rightField.damage(location-3, damage, own);
         return leftField.damage(location, damage, own);
+    }
+
+    public void newTurn(){
+        if (lowerHeroMaxMana < MAX_MANA)
+            lowerHeroMaxMana++;
+        lowerHeroMana = lowerHeroMaxMana;
     }
 }
